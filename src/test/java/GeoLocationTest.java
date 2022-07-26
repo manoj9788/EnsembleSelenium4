4302d996864edea8fec740c3aff500315c4a9035
Manoj Kumar <manojk@Manojs-MacBook-Pro-2.local>
2022-07-26 16:19:26 +0530

    Initial commit

---
+++ src/test/java/GeoLocationTest.java
@@ -0,0 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+
+public class GeoLocationTest {
+    ChromeDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    void teardown() {
+         driver.quit();
+    }
+
+    @Test
+    public void GeoLocationTest1(){
+
+    }
+}
